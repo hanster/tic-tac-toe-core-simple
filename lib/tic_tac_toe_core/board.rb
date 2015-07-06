9a559f7cb6ff87732201734066f62afed7ff130e
Sam Han <shan@8thlight.com>
2015-07-06 16:21:08 +0100

    Add board finish check

--- lib/tic_tac_toe_core/board.rb
+++ lib/tic_tac_toe_core/board.rb
@@ -1,6 +1,11 @@
 module TicTacToeCore
   class Board
     EMPTY_MARK = '-'
+    WINNING_ROWS = [
+      [0, 1, 2], [3, 4, 5], [6, 7, 8],
+    [0, 3, 6], [1, 4, 7], [2, 5, 8],
+    [0, 4, 8], [2, 4, 6]]
+
     attr_reader :positions
 
     def self.create_empty(size = 3)
@@ -10,6 +15,50 @@ module TicTacToeCore
 
     def initialize(positions)
       @positions = positions
+    end
+
+    def move(index, marker)
+      new_positions = positions.dup
+      new_positions[index] = marker
+      Board.new(new_positions)
+    end
+
+    def is_available?(index)
+      positions[index] == EMPTY_MARK
+    end
+
+    def available_moves
+      positions.each_index.select do |index|
+        is_available?(index)
+      end
+    end
+
+    def finished?
+      line_win = WINNING_ROWS.any? do |winning_row|
+        row_has_winner?(winning_row)
+      end
+      line_win || available_moves.length == 0
+    end
+
+    def all_moves
+      (0...positions.length).to_a
+    end
+
+    def marker_at(index)
+      positions[index]
+    end
+
+    private
+
+    def row_has_winner?(row)
+      (1...row.length).each do |index|
+        return false if positions[row[index]] == EMPTY_MARK || first_in_row(row) != positions[row[index]]
+      end
+      true
+    end
+
+    def first_in_row(row)
+      positions[row[0]]
     end
   end
 end
--- spec/tic_tac_toe_core/board_spec.rb
+++ spec/tic_tac_toe_core/board_spec.rb
@@ -4,11 +4,14 @@ require 'tic_tac_toe_core/board'
 module TicTacToeCore
   describe Board do
     let (:empty_mark) { Board::EMPTY_MARK }
+    let (:empty_three_board) { Board.create_empty }
+    let (:x_marker) { 'X' }
+    let (:o_marker) { 'O' }
+
     it "starts as an empty board" do
-      board = Board.create_empty
-      expect(board.positions).to eq([empty_mark, empty_mark, empty_mark,
-                                     empty_mark, empty_mark, empty_mark,
-                                     empty_mark, empty_mark, empty_mark])
+      expect(empty_three_board.positions).to eq([empty_mark, empty_mark, empty_mark,
+                                                 empty_mark, empty_mark, empty_mark,
+                                                 empty_mark, empty_mark, empty_mark])
     end
 
     it "starts as an empty 4x4 board" do
@@ -19,7 +22,123 @@ module TicTacToeCore
                                      empty_mark, empty_mark, empty_mark, empty_mark])
     end
 
-    it "" do
+    it "places a marker in the correct position" do
+      next_board = empty_three_board.move(0, x_marker)
+      expect(next_board.positions).to eq([x_marker, empty_mark, empty_mark,
+                                         empty_mark, empty_mark, empty_mark,
+                                         empty_mark, empty_mark, empty_mark])
+    end
+
+    it "places a marker which does not affect the previous board" do
+      next_board = empty_three_board.move(0, x_marker)
+      expect(empty_three_board.positions).to eq([empty_mark, empty_mark, empty_mark,
+                                                 empty_mark, empty_mark, empty_mark,
+                                                 empty_mark, empty_mark, empty_mark])
+
+      expect(next_board.positions).to eq([x_marker, empty_mark, empty_mark,
+                                         empty_mark, empty_mark, empty_mark,
+                                         empty_mark, empty_mark, empty_mark])
+    end
+
+    it "a position is not available once a move has been made there" do
+      next_board = empty_three_board.move(0, x_marker)
+      expect(next_board.is_available?(0)).to be false
+    end
+
+    it "returns all available moves" do
+      expect(empty_three_board.available_moves).to eq([0, 1, 2, 3, 4, 5, 6, 7, 8])
+    end
+
+    it "returns all available moves after a move has been made" do
+      next_board = empty_three_board.move(2, x_marker)
+      expect(next_board.available_moves).to eq([0, 1, 3, 4, 5, 6, 7, 8])
+    end
+
+    it "is not finished when you create an empty board" do
+      expect(empty_three_board.finished?).to be false
+    end
+
+    it "is finished when the board is full" do
+      board = Board.new([x_marker, x_marker, x_marker,
+                         x_marker, x_marker, x_marker,
+                         x_marker, x_marker, x_marker])
+      expect(board.finished?).to be true
+    end
+
+    it "is finished when the board has a winner" do
+      board = Board.new([x_marker, x_marker, x_marker,
+                         empty_mark, empty_mark, empty_mark,
+                         empty_mark, empty_mark, empty_mark])
+      expect(board.finished?).to be true
+    end
+
+    it "is not finished when the board has a row with an empty marker" do
+      board = Board.new([x_marker, empty_mark, x_marker,
+                         empty_mark, empty_mark, empty_mark,
+                         empty_mark, empty_mark, empty_mark])
+      expect(board.finished?).to be false
+    end
+
+    it "checks all the middle horizontal row will cause a finished board" do
+      board = Board.new([empty_mark, empty_mark, empty_mark,
+                         x_marker, x_marker, x_marker,
+                         empty_mark, empty_mark, empty_mark])
+      expect(board.finished?).to be true
+    end
+
+    it "checks all the bottom horizontal row will cause a finished board" do
+      board = Board.new([empty_mark, empty_mark, empty_mark,
+                         empty_mark, empty_mark, empty_mark,
+                         x_marker, x_marker, x_marker])
+      expect(board.finished?).to be true
+    end
+
+    it "checks all the left vertical row will cause a finished board" do
+      board = Board.new([x_marker, empty_mark, empty_mark,
+                         x_marker, empty_mark, empty_mark,
+                         x_marker, empty_mark, empty_mark])
+      expect(board.finished?).to be true
+    end
+
+    it "checks all the middle vertical row will cause a finished board" do
+      board = Board.new([empty_mark, x_marker, empty_mark,
+                         empty_mark, x_marker, empty_mark,
+                         empty_mark, x_marker, empty_mark])
+      expect(board.finished?).to be true
+    end
+
+    it "checks all the right vertical row will cause a finished board" do
+      board = Board.new([empty_mark, empty_mark, x_marker,
+                         empty_mark, empty_mark, x_marker,
+                         empty_mark, empty_mark, x_marker])
+      expect(board.finished?).to be true
+    end
+
+    it "checks all the top left to bottom right diagonal row will cause a finished board" do
+      board = Board.new([x_marker, empty_mark, empty_mark,
+                         empty_mark, x_marker, empty_mark,
+                         empty_mark, empty_mark, x_marker])
+      expect(board.finished?).to be true
+    end
+
+    it "checks all the top right to bottom left diagonal row will cause a finished board" do
+      board = Board.new([empty_mark, empty_mark, x_marker,
+                         empty_mark, x_marker, empty_mark,
+                         x_marker, empty_mark, empty_mark])
+      expect(board.finished?).to be true
+    end
+
+    it "returns all move indices" do
+      expect(empty_three_board.all_moves).to eq([0, 1, 2, 3, 4, 5, 6, 7, 8])
+    end
+
+    it "returns the marker at a specific position" do
+      next_board = empty_three_board.move(0, x_marker).move(1, o_marker)
+      expect(next_board.marker_at(0)).to eq(x_marker)
+      expect(next_board.marker_at(1)).to eq(o_marker)
+    end
+
+    it "returns the winner" do
 
     end
   end
